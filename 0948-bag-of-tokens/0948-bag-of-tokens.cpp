95b8c82e12e615cb6ec429e8b1ad6518b746f568
Aditya Mondal <130544041+CodingWizard07@users.noreply.github.com>
2024-03-05 20:54:24 +0530

    Time: 4 ms (75.27%), Space: 13.1 MB (56.36%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.cpp
+++ 0948-bag-of-tokens/0948-bag-of-tokens.cpp
@@ -1,28 +1,45 @@
+
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        sort(tokens.begin(), tokens.end());
-        int n = tokens.size();
-        int score = 0;
-        int max_score = 0;
-        int left = 0;
-        int right = n - 1;
+         // finding the array size
+    int n = tokens.size();
+
+    // initializing score
+    int max_score = 0;
+
+    // sorting the array
+    sort(begin(tokens),end(tokens));
+
+    int i = 0;
+    int j = n - 1;
         
-        while (left <= right) {
-            if (power >= tokens[left]) {
-                power -= tokens[left];
-                score += 1;
-                left += 1;
-                max_score = max(max_score, score);
-            } else if (score > 0) {
-                power += tokens[right];
-                score -= 1;
-                right -= 1;
-            } else {
-                break;
-            }
+    int score = 0;
+    while (i <= j)
+    {
+        // checking if power can be used to increase score
+        if (power >= tokens[i])
+        {
+            power -= tokens[i];
+            score += 1;
+            i++;
+            
+            max_score = max(max_score,score);
         }
-        
-        return max_score;
+        // checking if score need to be used to increase power
+        else if (score >= 1)
+        {
+            power += tokens[j];
+            score -= 1;
+            j--;
+        }
+        // if score is 0 and power is also less than tokens
+        else
+        {
+
+            return max_score;
+        }
+    }
+    return max_score;
     }
 };
